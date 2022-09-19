91450869fa17c7a2800eb2084dcb052ba7e4f1b3
heeyoung2da <mnkl1225@gmail.com>
2022-09-19 20:02:06 +0900

    react-for-beginners 파일 추가

---
+++ react-for-beginners/src/App_0.js
@@ -0,0 +1,13 @@
+import Button from './Button';
+import styles from "./App.module.css"
+
+function App_0() {
+  return (
+    <div>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <Button text={"Continue"} />
+    </div>
+  );
+}
+
+export default App_0;
