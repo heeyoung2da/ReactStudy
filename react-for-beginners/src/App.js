29d2708a3b5f50cb15c1f1ec8b2051483b121d88
heeyoung2da <mnkl1225@gmail.com>
2022-09-28 23:02:00 +0900

    react-for-beginners 파일 추가-Movie App

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,48 +1,7 @@
 // Movie App
-import { useState, useEffect } from "react";
 
 function App() {
-  const [loading, setLoding] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    // const response = await fetch(
-    //   "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-    // );
-    // const json = await response.json();
-    const json = await (
-      await fetch(
-        "https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year"
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoding(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((movie) => (
-            <div key={movie.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
