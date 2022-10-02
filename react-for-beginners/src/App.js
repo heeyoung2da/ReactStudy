a042689b10b4b75c6324f9ba4b3a927d6528b63f
heeyoung2da <mnkl1225@gmail.com>
2022-10-02 21:56:31 +0900

    react-for-beginners 파일 추가-Movie App

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,7 +1,17 @@
 // Movie App
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 function App() {
-  return null;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/hello" element={<h1>Hello</h1>}></Route>
+        <Route path="/movie" element={<Detail />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- react-for-beginners/src/components/Movie.js
+++ react-for-beginners/src/components/Movie.js
@@ -1,10 +1,13 @@
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>{genres && genres.map((g) => <li key={g}>{g}</li>)}</ul>
     </div>
@@ -15,7 +18,7 @@ Movie.propTypes = {
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
-  genres: PropTypes.arrayOf(PropTypes.string).isRequired, //arrayOf(PropTypes)	해당 PropTypes으로 구성된 배열
+  genres: PropTypes.arrayOf(PropTypes.string), //arrayOf(PropTypes)	해당 PropTypes으로 구성된 배열
 };
 
 export default Movie;
