483360d5b6cad04d9c54f28eacb81a648e84a489
heeyoung2da <mnkl1225@gmail.com>
2022-10-04 20:29:32 +0900

    react-for-beginners 파일 수정-Movie App

--- react-for-beginners/src/routes/Detail.js
+++ react-for-beginners/src/routes/Detail.js
@@ -1,5 +1,31 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [loading, setLoding] = useState(true);
+  const [detail, setDetail] = useState([]);
+  const getMovie = async () => {
+    const json = await (
+      await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+    ).json();
+    setDetail(json.data.movie);
+    setLoding(false);
+  };
+  useEffect(() => {
+    getMovie();
+  }, []);
+  return (
+    <div>
+      {loading ? (
+        <h1> Loading...</h1>
+      ) : (
+        <div>
+          <h1>Detail</h1>
+          {detail.description_full}
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
